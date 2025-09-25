5ad1ebd55b9dd91a93c78fffc6d5027f3352543f
Gerben Snellenberg <gerben.snellenberg@edsn.nl>
2025-09-25 13:04:30 +0200

    added s3 plugin

--- config/general.php
+++ config/general.php
@@ -23,7 +23,7 @@ return GeneralConfig::create()
     ->preventUserEnumeration()
     // Set the @webroot alias so the clear-caches command knows where to find CP resources
     ->aliases([
-        '@webroot' => App::env('WEB_ROOT_PATH'),
+        '@webroot' => dirname(__DIR__) . '/web',
         '@uploads' => App::env('PRIMARY_SITE_URL') . '/uploads',
     ])
     ->headlessMode(true)
